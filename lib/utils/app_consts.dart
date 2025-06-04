69cf7cfde48815a5d83bc7dca2301562d5b01ac1
Asad Ali <it@smuccivapes.com>
2025-06-04 12:40:12 +0400

    const

--- lib/utils/app_consts.dart
+++ lib/utils/app_consts.dart
@@ -1,16 +1,6 @@
 class AppConsts {
   static String apiKey = "";
   static const String baseUrl ='https://dummyjson.com/';
-  static const String baseUrlImage ='https://api.smuccidrop.com/public/';
-  static const String proImageUrl = '${baseUrlImage}uploads/product_images/';
-  static const String cateImageUrl = '${baseUrlImage}uploads/category_images/';
-  static const String recycleImagesUrl = '${baseUrlImage}uploads/recycle_images/';
-  static const String customerProfileImagesUrl = '${baseUrlImage}uploads/userprofile/';
-  static const String profileImagesUrl = '${baseUrlImage}uploads/store_images/';
-  static const String eidImagesUrl = '${baseUrlImage}uploads/driver_gov_id_images/';
-  static const String bannerImagesUrl = '${baseUrlImage}uploads/banner/';
-  static const String nearestRecycle = '${baseUrlImage}uploads/all_images_recycle/';
-
   /* -------------------------------------------------------------------------- */
   /*                                 ApiData                                    */
   /* -------------------------------------------------------------------------- */
